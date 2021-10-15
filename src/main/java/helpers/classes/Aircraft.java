2df175b946c3db2f07af271e3c79ee526d20e93b
JorBen11 <87387846+JorBen11@users.noreply.github.com>
2021-10-14 23:25:17 -0500

    Implementacion de lombok

--- src/main/java/helpers/classes/Aircraft.java
+++ src/main/java/helpers/classes/Aircraft.java
@@ -1,8 +1,11 @@
 package helpers.classes;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class Aircraft {
-    private int ID_aircraft;
+	private int ID_aircraft;
     private String type;
     private String name;
     private String model;
@@ -11,68 +14,5 @@ public class Aircraft {
     private int ID_airline;
     private int typeallowed;
 
-    public int getID_aircraft() {
-        return this.ID_aircraft;
-    }
-
-    public void setID_aircraft(int ID_aircraft) {
-        this.ID_aircraft = ID_aircraft;
-    }
-
-    public String getType() {
-        return this.type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getModel() {
-        return this.model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public int getCapacity() {
-        return this.capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public double getRange() {
-        return this.range;
-    }
-
-    public void setRange(double range) {
-        this.range = range;
-    }
-
-    public int getID_airline() {
-        return this.ID_airline;
-    }
-
-    public void setID_airline(int ID_airline) {
-        this.ID_airline = ID_airline;
-    }
-
-    public int getTypeallowed() {
-        return this.typeallowed;
-    }
-
-    public void setTypeallowed(int typeallowed) {
-        this.typeallowed = typeallowed;
-    }
-
 }
+
